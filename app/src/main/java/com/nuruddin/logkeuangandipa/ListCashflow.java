85e0ff3e4152ce4daf9b9c6ca1c0c665ca86252c
nrdnisml <nurudinismail69@gmail.com>
2022-09-16 14:37:11 +0700

    finsih project

---
+++ app/src/main/java/com/nuruddin/logkeuangandipa/ListCashflow.java
@@ -0,0 +1,14 @@
+package com.nuruddin.logkeuangandipa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListCashflow extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_cashflow);
+    }
+}
